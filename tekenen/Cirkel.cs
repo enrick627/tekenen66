61caac42c8dd24e3ae62c6026110bfe22b251ceb
enrick627 <enrick.demunter@student.romerocollege.be>
2020-05-09 18:11:13 +0200

    Add project files.

---
+++ tekenen/Cirkel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tekenen
+{
+    public class Cirkel
+    {
+        /// <summary>
+        /// de straal uitgedrukt in pixels
+        /// </summary>
+        public int Straal
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
